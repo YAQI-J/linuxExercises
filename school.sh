42706a20777490320c98273c47fc0664386d72a8
Iris Chow <tchow7@wisc.edu>
2022-03-08 11:53:16 -0600

    Commented out steps

--- school.sh
+++ school.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-cat Property_Tax_Roll.csv | head -n 20
-grep -h "MADISON SCHOOLS" Property_Tax_Roll.csv | head -n 20
-cut Property_Tax_Roll.csv -f 7 | head -n 20
-head Property_Tax_Roll.csv | grep -h "MADISON SCHOOLS" | cut -d, -f 7 | { sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$(($count + 1)); done; avg=$(($sum / $count)); echo $avg; }
+
+# I used these steps to develop my working code:
+#   cat Property_Tax_Roll.csv | head -n 20
+#   grep -h "MADISON SCHOOLS" Property_Tax_Roll.csv | head -n 20
+#   cut Property_Tax_Roll.csv -f 7 | head -n 20
+#   head Property_Tax_Roll.csv | grep -h "MADISON SCHOOLS" | cut -d, -f 7 | { sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$(($count + 1)); done; avg=$(($sum / $count)); echo $avg; }
 
 
 cat Property_Tax_Roll.csv | grep -h "MADISON SCHOOLS" | cut -d, -f 7 | { sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$(($count + 1)); done; avg=$(($sum / $count)); echo $avg; }
