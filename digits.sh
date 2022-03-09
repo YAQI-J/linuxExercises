4db301457fc66f2fb0fdd7d527a1f97d1964db5f
Iris Chow <tchow7@wisc.edu>
2022-03-09 08:45:05 -0600

    fixed digits.sh

--- digits.sh
+++ digits.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-pattern="[01]\{4\}"
+pattern="[01]{4}"
 
 sum=0;
 for n in {1000..2000}; do
+#for n in {1000..1003}; do    
     if [[ $n =~ $pattern ]]; then
 	sum=$(($sum + $n));
     fi;
