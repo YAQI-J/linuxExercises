3450116ff5e8a2a299495853e133fdc5d7e23b3e
Yaqi Jie <yjie2@wisc.edu>
2022-03-10 10:37:51 -0600

    to be developed

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# make a directory five
+mkdir five
+# make five subdirectories
+for i in 1 2 3 4 5
+do
+    mkdir five/dir$i
+    for j in 1 2 3 4
+    do
+	> ../dir$i/file$j
+	echo {1..$j} > file$j
+    done
+done
+
