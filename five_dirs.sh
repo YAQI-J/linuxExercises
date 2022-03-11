a7edcd7c44d4d06cbb51b93d35b3cbd516677e49
Yaqi Jie <yjie2@wisc.edu>
2022-03-10 18:58:54 -0600

    finished

--- five_dirs.sh
+++ five_dirs.sh
@@ -8,8 +8,11 @@ do
     mkdir five/dir$i
     for j in 1 2 3 4
     do
-	> ../dir$i/file$j
-	echo {1..$j} > file$j
+	> five/dir$i/file$j
+	for (( k = 1 ; k <= j ; k++ ))
+	do
+	    echo $j >> five/dir$i/file$j
+	done
     done
 done
 
