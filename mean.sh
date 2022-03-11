932d5971e751734617b4b279ab5eabd60975f033
Ruohe Zhou <rzhou73@wisc.edu>
2022-03-11 14:39:14 -0600

    first_mean_version

--- mean.sh
+++ mean.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo 'usage: $0 $1 $2' | cat $2 |
+    cut -c$1 |tail -n +2 |
+    { count=1;
+      while read n;
+      do
+	  count=$(($count + 1));
+      done;
+      awk '{ sum += $1 } END {print sum}' [$2];
+      mean=$(($sum / $count));
+      echo $mean; }
